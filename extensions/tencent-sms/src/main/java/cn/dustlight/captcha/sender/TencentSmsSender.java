6e3a6a770cc10c244919f4b56a541d733e84152e
Hansin1997 <845612500@qq.com>
2020-12-18 01:23:08 +0800

    tencent-sms 允许设置手机号参数名，默认为'phone'

--- extensions/tencent-sms/src/main/java/cn/dustlight/captcha/sender/TencentSmsSender.java
+++ extensions/tencent-sms/src/main/java/cn/dustlight/captcha/sender/TencentSmsSender.java
@@ -12,8 +12,8 @@ import java.util.Map;
 
 public class TencentSmsSender implements CodeSender<String> {
 
-    private TencentSmsProperties properties;
-    private SmsClient client;
+    private final TencentSmsProperties properties;
+    private final SmsClient client;
 
     public TencentSmsSender(TencentSmsProperties properties) {
         this.properties = properties;
@@ -54,9 +54,12 @@ public class TencentSmsSender implements CodeSender<String> {
             request.setSenderId(getSenderId(parameters));
             request.setTemplateID(getTemplateId(parameters));
 
-            if (!parameters.containsKey("phone") || parameters.get("phone") == null)
-                throw new SendCodeException("Parameter 'phone' not found!");
-            request.setPhoneNumberSet(new String[]{parameters.get("phone").toString()});
+            String phoneParamName = getPhoneParamName(parameters);
+
+            if (!parameters.containsKey(phoneParamName) || parameters.get(phoneParamName) == null)
+                throw new SendCodeException(String.format("Parameter '%s' not found!", phoneParamName));
+
+            request.setPhoneNumberSet(new String[]{parameters.get(phoneParamName).toString()});
             String[] paramSet = getParamSet(parameters);
             if (paramSet == null)
                 paramSet = new String[]{code.getValue()};
@@ -76,7 +79,7 @@ public class TencentSmsSender implements CodeSender<String> {
         String[] paramName = parameters.get("PARAMS").toString().split(",");
         String[] values = new String[paramName.length];
         for (int i = 0; i < paramName.length; i++) {
-            if (parameters.get(paramName[i]) == null)
+            if (!parameters.containsKey(paramName[i]) || parameters.get(paramName[i]) == null)
                 continue;
             values[i] = parameters.get(paramName[i]).toString();
         }
@@ -97,5 +100,9 @@ public class TencentSmsSender implements CodeSender<String> {
 
     protected String getTemplateId(Map<String, Object> parameters) {
         return (String) parameters.getOrDefault("TEMPLATE_ID", properties.getDefaultTemplateId());
+    }
+
+    protected String getPhoneParamName(Map<String, Object> parameters) {
+        return (String) parameters.getOrDefault(properties.getPhoneParamName(), "phone");
     }
 }
